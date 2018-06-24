f6c181fd5322233e480536c246b8969a2ce77264
Gabriel Vance <ilovenarwhales@gmail.com>
2018-06-24 20:51:40 +0300

    adding python

---
+++ week-8/Vance_calculator.py
@@ -0,0 +1,20 @@
+first_name='Gabriel'
+last_name='Vance'
+print(first_name+' ' +last_name)
+assignment='8.3'
+print('Assignment'+' '+assignment)
+
+
+# add function with 2 params
+def add(x,y):
+    return (x+y)
+# subtract function with 2 params
+def subtract(x,y):
+    return (x-y)
+# divide function with 2 params
+def divide(x,y):
+    return (x/y)
+
+print(add(1,2))
+print(subtract(4,1))
+print(divide(8,2))
